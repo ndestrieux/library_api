78ee3dbddd4994c6aab804595246f547dd97c365
ndestrieux <n.destrieux@gmail.com>
2022-06-21 13:32:15 +0200

    Models added to admin view

--- backend/api_app/admin.py
+++ backend/api_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from api_app.models import Author, Publisher, Book
+
+admin.site.register(Author)
+admin.site.register(Publisher)
+admin.site.register(Book)
