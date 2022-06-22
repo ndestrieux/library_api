981bdf4cb0dc4f176a26cae8ca3d8d0a56ac2488
ndestrieux <n.destrieux@gmail.com>
2022-06-22 19:17:20 +0200

    Models updated, manytomany field relationship was moved from Book to Author

--- backend/api_app/models.py
+++ backend/api_app/models.py
@@ -1,19 +1,6 @@
 from django.db import models
 
 # Books
-
-class Author(models.Model):
-    first_name = models.CharField(max_length=64)
-    last_name = models.CharField(max_length=64)
-
-    @property
-    def name(self):
-        return f"{self.first_name} {self.last_name}"
-
-    def __str__(self):
-        return self.name
-
-
 class Publisher(models.Model):
     name = models.CharField(max_length=64)
 
@@ -23,7 +10,6 @@ class Publisher(models.Model):
 
 class Book(models.Model):
     title = models.CharField(max_length=128)
-    author = models.ManyToManyField(Author)
     publication_year = models.IntegerField(null=True, blank=True)
     publisher = models.ForeignKey(Publisher, blank=True, null=True, on_delete=models.SET_NULL)
 
@@ -31,3 +17,14 @@ class Book(models.Model):
         return self.title
 
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    books = models.ManyToManyField(Book)
+
+    @property
+    def name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return self.name
